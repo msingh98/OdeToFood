a6f98c376672288310b5cb5e6a07ddb1a1ccbef9
msingh98 <msingh@macalester.edu>
2020-10-06 14:40:07 -0500

    setting up restaurant and cuisine classes

---
+++ OdeToFood/OdeToFood.Data/Models/CuisinType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Models
+{
+    public enum CuisinType
+    {
+        None,
+        Italian,
+        Indian,
+        French
+    }
+}
---
+++ OdeToFood/OdeToFood.Data/Models/Restaurant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Models
+{
+    class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public CuisinType Cuisine { get; set; }
+    } 
+}
