4e52feba83c0b672545f0ce4e9c5d14efb5ac266
msingh98 <msingh@macalester.edu>
2020-10-06 21:55:35 -0500

    create/update + validation

--- OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -14,15 +14,36 @@ namespace OdeToFood.Data.Services
             restaurants = new List<Restaurant>()
             {
                 new Restaurant { Id = 1, Name = "Shish", Cuisine = CuisineType.Mediterranean},
-                new Restaurant { Id = 1, Name = "Himalayan", Cuisine = CuisineType.Nepali},
-                new Restaurant { Id = 1, Name = "Chipotle", Cuisine = CuisineType.Mexican }
+                new Restaurant { Id = 2, Name = "Himalayan", Cuisine = CuisineType.Nepali},
+                new Restaurant { Id = 3, Name = "Chipotle", Cuisine = CuisineType.Mexican }
             };
 
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            restaurants.Add(restaurant);
+            restaurant.Id = restaurants.Max(restaurants => restaurants.Id) + 1;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return restaurants.FirstOrDefault(r => r.Id == id);
         }
 
         public IEnumerable<Restaurant> GetAll()
         {
             return restaurants.OrderBy(r=> r.Name);
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            var existing = Get(restaurant.Id);
+            if(existing !=null)
+            {
+                existing.Name = restaurant.Name;
+                existing.Cuisine = restaurant.Cuisine;
+            }    
         }
     }
 }
---
+++ OdeToFood/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -0,0 +1,80 @@
+﻿using OdeToFood.Data.Models;
+using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        private readonly IRestaurantData db;
+        public RestaurantsController(IRestaurantData db)
+        {
+            this.db = db;
+        }
+        // GET: Restaurants
+        public ActionResult Index()
+        {
+            var model = db.GetAll();
+            return View(model);
+        }
+        public ActionResult Details(int id)
+        {
+            var model = db.Get(id);
+            if(model == null)
+            {
+                return View("NotFound");
+            }
+            return View(model);
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Add(restaurant);
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }    
+            
+            return View();
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var model = db.Get(id);
+            if (model == null)
+            {
+                return View("NotFound");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Update(restaurant);
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }
+
+            return View();
+
+        }
+
+
+    }
+}
